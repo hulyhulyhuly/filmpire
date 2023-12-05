3ff5ecdfe164043acc03221ac8dc8748a3fb9581
huly <qq6308qq@gmail.com>
2023-12-05 15:35:40 +0800

    fix: minor fixes

--- src/components/RatedCards/RatedCards.jsx
+++ src/components/RatedCards/RatedCards.jsx
@@ -6,7 +6,7 @@ import { Movie } from '..';
 const RatedCards = ({ title, data }) => (
   <Box>
     <Typography variant="h5" gutterBottom>{title}</Typography>
-    <Box display="flex" flexWrap="wrap" className={cls.ratedContainer}>
+    <Box display="flex" flexWrap="wrap">
       { data?.results.map((m, i) => <Movie key={m.id} movie={m} i={i} />) }
     </Box>
   </Box>
